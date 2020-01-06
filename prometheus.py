2260431b0ad3b49059e13c21a03e199a00fbc3f6
Capprin Bass <capprin@gmail.com>
2020-01-05 23:16:06 -0700

    Added start, stop, restart commands

--- prometheus.py
+++ prometheus.py
@@ -1,18 +1,7 @@
 import sys, os, math, json, zipfile, click, boto3
 
-# workflow:
-#    create instances. Need:
-#        - ami (option, with default)
-#        - type (option, with default. Also provide a reference)
-#        - keypair (generated for each instance, stored in local file)
-#       will produce ec2 instance we can access. We store necessary information in a file in provided directory
-#    push program to instances
-#    wait for program to end
-#       include interrupted behavior (close instance?)
-#    download program data
-#    terminate instance
-
-#TODO: Figure out how to do more robust permissions checks (with dryrun maybe)
+# PROMETHEUS
+# instance-manager: only for managing ec2 instances
 
 # computes directory size, in bytes. Probably shouldn't be used.
 def get_sub_items(start_path = '.'):
@@ -36,15 +25,12 @@ def cli():
 #TODO: Make these options choices (how should I do this so they're not super insane?)
 @cli.command()
 @click.option('-k', '--key-name', 'keypair', required=True, help='amazon keypair used to access instance')
-@click.option('-l', '--key-path', 'keypath', required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False), help='location of ssh key')
 @click.option('-s', '--size', type=click.INT, help = 'EBS size, in GB. Highly Recommended!')
 @click.option('-t', '--type', default='t2.micro', help='instance type')
 @click.option('-a', '--ami', default='ami-08d489468314a58df', help='amazon machine image')
 @click.option('-g', '--security-group-name', 'securitygroup', help='provided security group. MUST allow ssh from this machine.')
-@click.option('-r', '--save', help='save instance information after start', flag_value=True)
-@click.option('-p', '--persist', help='keep instance running', flag_value=True)
 @click.argument('directory', type=click.Path(exists=True, file_okay=False, dir_okay=True), default='.')
-def run (directory, keypair, keypath, size, ami, securitygroup, type, save, persist):
+def start (directory, keypair, size, ami, securitygroup, type):
     """Creates and runs an AWS EC2 instance that runs the process stored in DIRECTORY."""
 
     # create ec2 resource; main workhorse
@@ -151,81 +137,69 @@ def run (directory, keypair, keypath, size, ami, securitygroup, type, save, pers
         instance.wait_until_running()
         instance.reload()
 
-    # save instance information if required
-    if save or persist:
-        click.echo('Writing instance information to ' + directory + '/instance-info.json')
-        instance_info = []
-        for instance in instance_list:
-            info = {
-                'id' : instance.id,
-                'ami' : instance.image_id,
-                'type': instance.instance_type,
-                'key-name' : instance.key_name,
-                #'launch-time' : instance.launch_time,
-                'public-dns' : instance.public_dns_name,
-                'public-ip' : instance.public_ip_address
-            }
-            instance_info.append(info)
-        out_file = open(directory + '/instance-info.json','w')
-        out_file.write(json.dumps(instance_info))
-        out_file.close()
-
-    # for now, using one instance
-    instance = instance_list[0]
-
-    # zip working directory
-    #click.echo("Compressing working directory")
-    #files = get_sub_items(directory) #TODO: Decide if we should just do this once
-    #zip_file = zipfile.ZipFile('prometheus.zip','w') #this creates a file in the same folder as prometheus (not desired)
-    #with zip_file:
-    #    for f in files:
-    #        zip_file.write(f)
-    #    zip_file.close()
-
-    #TODO: Start main process
-    #   - wait until exit (normally):
-    #       - if success, continue normally
-    #       - if failure, prompt for persistence (for debug)
-    #   - don't wait (optional parameter, maybe)
-
-    #TODO: Move directory contents from instance
-    #   - only if done with the instance
-
-    #TODO: Better behavior than exit if we've already created the instance (terminate it)
-    # terminate ec2 instance if we're supposed to
-    if not persist:
-        try:
-            for instance in instance_list:
-                instance.terminate()
-        except:
-            #TODO: Handle this better. Maybe wait?
-            click.echo('Failed to terminate ec2 instance')
-            exit(1)
-        else:
-            click.echo('Terminated instance successfully.')
-    else:
-        click.echo('Persisting ec2 instance.')
+    # save instance information
+    click.echo('Writing instance information to ' + directory + '/instance-info.json')
+    instance_info = []
+    for instance in instance_list:
+        info = {
+            'id' : instance.id,
+            'ami' : instance.image_id,
+            'type': instance.instance_type,
+            'key-name' : instance.key_name,
+            'public-dns' : instance.public_dns_name,
+            'public-ip' : instance.public_ip_address
+        }
+        instance_info.append(info)
+    out_file = open(directory + '/instance-info.json','w')
+    out_file.write(json.dumps(instance_info))
+    out_file.close()
 
     click.echo('Done.')
 
+# restarts stopped ec2 instances
+@cli.command()
+@click.argument('instance_id', nargs=-1)
+def restart(instance_id):
+    """Restarts stopped instances with provided INSTANCE_ID's"""
+    click.echo('Restarting instance(s)...')
+    try:
+        ec2 = boto3.client('ec2')
+        ec2.start_instances(InstanceIds = list(instance_id))
+    except:
+        click.echo('Failed to restart instances.')
+    else:
+        click.echo('Start command sent to aws successfully.')
+    click.echo('Done.')
 
 # stops instances with provided ids
 @cli.command()
 @click.argument('instance_id', nargs=-1)
-def stop(instance_id):
-    """Stops running instances with provided INSTANCE_ID's."""
+def stop (instance_id):
+    """Stops running instances with provided INSTANCE_ID's"""
+    click.echo('Stopping instance(s)...')
+    try:
+        ec2 = boto3.client('ec2')
+        ec2.stop_instances(InstanceIds = list(instance_id))
+    except:
+        click.echo('Failed to stop instances.')
+    else:
+        click.echo('Stop command sent to aws successfully.')
+    click.echo('Done.')
+
+# terminates instances with provided ids
+@cli.command()
+@click.argument('instance_id', nargs=-1)
+def terminate(instance_id):
+    """Terminates running instances with provided INSTANCE_ID's."""
     click.echo('Terminating instance(s)...')
     try:
         ec2 = boto3.client('ec2')
         ec2.terminate_instances(InstanceIds = list(instance_id))
     except:
         click.echo('Failed to terminate instances.')
+    else:
+        click.echo('Terminate command sent to aws successfully.')
     click.echo('Done.')
-
-#TODO: figure out reference
-#   - instance types overview/reference?
-#   - brief ami reference?
-
 
 # start cli
 if __name__ == '__main__':
